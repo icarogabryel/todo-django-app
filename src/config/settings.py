560b7f0c5f95a08a5444e9ec2e5c0f20f1f2a7e2
Infinite Monkey <icarogabryel2001@gmail.com>
2024-12-14 11:50:57 -0300

    task class and db created

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7!9s+niqztem)clu@cph0o@%$_$_w*(01++=o!+4jbdjl&chbh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'task.db',
     }
 }
 
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    color = models.CharField(max_length=10, default='#FFFFFF')
